027108023c63d4d88340cfbdd7b95e61417b374b
Chad Collard <chadcollard1567@gmail.com>
2025-07-21 17:40:18 -0400

    chapter 15 cpx part 1&2 done

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Chad Collard
-# Chapter 15 cpx 1
+# Chapter 15 cpx 1&2
 # Main
 # 7/20/2025
 
@@ -7,19 +7,25 @@ from adafruit_circuitplayground import cp
 import time
 from region import Region
 
-yellow = Region((255, 255, 0), (5, 6, 7))
-blue = Region((0, 0, 255), (2, 3, 4))
+REGION_DATA = {
+    "yellow": {"color": (255, 255, 0), "leds": (5, 6, 7), "tone": 252},
+    "blue": {"color": (0, 0, 255), "leds": (2, 3, 4), "tone": 209},
+    "red": {"color": (255, 0, 0), "leds": (7, 8, 9), "tone": 310},
+    "green": {"color": (0, 255, 0), "leds": (0, 1, 2), "tone": 415},
+}
+
+regions = [
+    Region(data["color"], data["leds"], data["tone"]) for data in REGION_DATA.values()
+]
 
 while True:
-    yellow.all_on()
-    cp.pixels.show()
-    time.sleep(0.5)
+    for region in regions:
+        region.all_on()
+        region.play_tone()
+        cp.pixels.show()
+        time.sleep(0.5)
 
-    yellow.all_off()
-    blue.all_on()
-    cp.pixels.show()
-    time.sleep(0.5)
-
-    blue.all_off()
-    cp.pixels.show()
-    time.sleep(0.5)
+        region.all_off()
+        region.stop_tone()
+        cp.pixels.show()
+        time.sleep(0.5)
--- region.py
+++ region.py
@@ -1,15 +1,20 @@
 # Chad Collard
-# Chapter 15 cpx 1
+# Chapter 15 cpx 1&2
 # Region class
 # 7/20/2025
 from adafruit_circuitplayground import cp
 
 
 class Region:
-    def __init__(self, color, leds,):
+    def __init__(
+        self,
+        color,
+        leds,
+        tone,
+    ):
         self.color = color
         self.leds = leds
-
+        self.tone = tone
 
     def set_color(self, color):
         self.color = color
@@ -17,18 +22,28 @@ class Region:
     def set_leds(self, leds):
         self.leds = leds
 
+    def set_tone(self, tone):
+        self.tone = tone
+
     def get_color(self):
         return self.color
 
     def get_leds(self):
         return self.leds
 
+    def get_tone(self):
+        return self.tone
+
     def all_on(self):
         for led_index in self.leds:
             cp.pixels[led_index] = self.color
-
 
     def all_off(self):
         for led_index in self.leds:
             cp.pixels[led_index] = (0, 0, 0)
 
+    def play_tone(self):
+        cp.start_tone(self.tone)
+
+    def stop_tone(self):
+        cp.stop_tone()
